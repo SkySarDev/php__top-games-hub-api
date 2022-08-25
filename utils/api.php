ec03911731671e08840be8679cfb2fb55c37b62a
SkySarDev <skysar@ya.ru>
2022-08-25 16:16:12 +0300

    update: change curl to GuzzleHttp

--- utils/api.php
+++ utils/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise;
+use GuzzleHttp\Exception\BadResponseException;
+
 function getUrl(string $category, string $query): string {
   $api_url = getenv('API_URL');
   $api_key = getenv('API_KEY');
@@ -7,65 +11,45 @@ function getUrl(string $category, string $query): string {
   return $api_url . $category . '?key=' . $api_key . '&' . $query;
 }
 
-function fetchData(string $url): mixed {
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_HEADER, false);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-  curl_setopt($ch, CURLOPT_FAILONERROR, true);
+function responseError(BadResponseException $ex): array {
+  $status = $ex->getCode();
+  $message = $ex->getResponse()->getBody()->getContents();
 
-  $response = curl_exec($ch);
+  http_response_code($status);
 
-  curl_close($ch);
+  return [
+    'error' => $status,
+    'message' => $message
+  ];
+}
 
-  if (curl_errno($ch)) {
-    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+function fetchData(string $url): array {
+  $client = new Client();
 
-    return [
-      'error' => $status_code,
-      'message' => curl_error($ch)
-    ];
-  } else {
-    return json_decode($response, true);
+  try {
+    $response = $client->get($url);
+
+    return json_decode($response->getBody(), true);
+  } catch (BadResponseException $ex) {
+    return responseError($ex);
   }
 }
 
 function fetchMultiData(array $urls): array {
-  $res = array();
-  $conn = array();
-  $mh = curl_multi_init();
+  $client = new Client();
+  $promises = [];
 
   foreach ($urls as $i => $url) {
-    $conn[$i] = curl_init();
-    curl_setopt($conn[$i], CURLOPT_URL, $url);
-    curl_setopt($conn[$i], CURLOPT_HEADER, 0);
-    curl_setopt($conn[$i], CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($conn[$i], CURLOPT_TIMEOUT, 10);
-    curl_setopt($conn[$i], CURLOPT_FOLLOWLOCATION, 1);
-    curl_multi_add_handle($mh, $conn[$i]);
+    $promises[$i] = $client->getAsync($url);
   }
 
-  $active = 0;
+  try {
+    $responses = Promise\Utils::unwrap($promises);
 
-  do {
-    $mrc = curl_multi_exec($mh, $active);
-  } while ($mrc == CURLM_CALL_MULTI_PERFORM);
-
-  while ($active && $mrc == CURLM_OK) {
-    if (curl_multi_select($mh) != -1) {
-      do {
-        $mrc = curl_multi_exec($mh, $active);
-      } while ($mrc == CURLM_CALL_MULTI_PERFORM);
-    }
+    return array_map(function ($response) {
+      return json_decode($response->getBody(), true);
+    }, $responses);
+  } catch (BadResponseException $ex) {
+    return responseError($ex);
   }
-
-  foreach ($urls as $i => $url) {
-    $res[$i] = json_decode(curl_multi_getcontent($conn[$i]), true);
-    curl_multi_remove_handle($mh, $conn[$i]);
-    curl_close($conn[$i]);
-  }
-  curl_multi_close($mh);
-
-  return $res;
 }
