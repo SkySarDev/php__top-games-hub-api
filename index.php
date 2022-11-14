ae479c4b6b8095c8e42654ff02e16f7c3771b3ca
SkySarDev <skysar@ya.ru>
2022-11-14 11:14:36 +0300

    fix: fix include file AppCache

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/configs/headers.php';
 require_once __DIR__ . '/utils/services-utils.php';
 require_once __DIR__ . '/utils/app.php';
 require_once __DIR__ . '/utils/api.php';
+require_once __DIR__ . '/app/cache.php';
 require_once __DIR__ . '/app/controller.php';
 require_once __DIR__ . '/app/routes.php';
-require_once __DIR__ . '/app/cache.php';
 
